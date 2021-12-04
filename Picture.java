ad86359fa05318deab4f4b99649b9b4d98c6e910
mihaelapocanschi <73704720+mihaelapocanschi@users.noreply.github.com>
2021-12-04 12:21:57 +0200

    Add files via upload
    Lab4 implementation

---
+++ Picture.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.TimeUnit;
+
+abstract class Picture implements Element {
+    public Element get(int elementIndex) {
+        return null;
+    }
+
+    public void add(Element element) {
+    }
+
+    public void remove(Element element) {
+    }
+
+    public void print() {
+    }
+}
